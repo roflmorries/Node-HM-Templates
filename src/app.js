6258f2eb45983c76c6529877d3503e9b0d072f54
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-07 23:49:06 +0300

    updated app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import path from 'node:path';
 import usersRouter from './routes/userRouter.js'
+import articleRouter from './routes/articleRouter.js'
+import ejs from 'ejs'
 
 const __dirname = import.meta.dirname;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pug'));
+
+app.engine('ejs', ejs.renderFile);
+app.set('views-ejs', path.join(__dirname, 'views/ejs'));
 
 
 app.use('/users', usersRouter);
+app.use('/articles', articleRouter);
 
 
 const PORT = 3000;
